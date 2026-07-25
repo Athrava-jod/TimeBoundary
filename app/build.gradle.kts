248b889573453b62ae3dac97459060cdd33d064f
Athrava-jod <atharvabhojane14@gmail.com>
2026-07-25 22:36:01 +0530

    change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.timeboundary.app"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.timeboundary.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
 
